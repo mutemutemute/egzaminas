adff6f07296f4026a47278ceb49f286884728c87
Laima <kulbyte.laima@gmail.com>
2025-02-28 09:28:04 +0200

    initial commit

---
+++ front/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./components/Home";
+import LoginForm from "./components/LoginForm";
+import SignupForm from "./components/SignupForm";
+import NotFound from "./components/NotFound";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/signup" element={<SignupForm />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
